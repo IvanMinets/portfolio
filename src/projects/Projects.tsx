d9453fde6ca2b0c61cde25d93c13d86ceec45f37
Ivan Minets <minetssmash@gmail.com>
2023-06-18 20:59:39 +0300

    css updates

---
+++ src/projects/Projects.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styles from './Projects.module.css'
+import styleContainer from "../common/styles/Container.module.css";
+import Project from "./project/project";
+
+
+const Projects = () => {
+    return (
+        <div className={styles.projectsBlock}>
+            <div className={`${styleContainer.container} ${styles.projectsContainer}`}>
+                <h2 className={styles.title}>Projects</h2>
+                <div className={styles.projects}>
+                    <Project title="Todolist" description="Todolist bla bla bla"/>
+                    <Project title="Social Network" description="Social network bla bla bla"/>
+                    <Project title="Counter" description="Counter bla bla bla"/>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Projects;
---
+++ src/projects/project/project.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import styles from './project.module.css'
+
+type ProjectPropsType = {
+    title: string
+    description: string
+}
+
+const Project = (props: ProjectPropsType) => {
+    return (
+        <div className={''}>
+            <div>
+                <div className={''}></div>
+                <button>Watch</button>
+            </div>
+            <div>
+                <h3>{props.title}</h3>
+                <span className={''}>{props.description}</span>
+            </div>
+        </div>
+    );
+};
+
+export default Project;
