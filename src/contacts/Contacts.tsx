60e1c2a25c75bbadd432344094ccb1ef18163ac4
Ivan Minets2 <minetssmash@gmail.com>
2023-06-19 12:42:56 +0300

    css updates, added footer

--- src/contacts/Contacts.tsx
+++ src/contacts/Contacts.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import style from './Contacts.module.css'
+import styleContainer from "../common/styles/Container.module.css";
 
 const Contacts = () => {
     return (
-        <div>
-            
+        <div className={style.contactsBlock}>
+            <div className={`${styleContainer.container} ${style.contactsContainer}`}>
+                <h2 className={style.title}>Contacts</h2>
+                <form className={style.contactsForm}>
+                    <input type="text"/>
+                    <input type="text"/>
+                    <textarea></textarea>
+                </form>
+                <a href="#" className={style.sendButton}>Отправить</a>
+            </div>
         </div>
     );
 };
