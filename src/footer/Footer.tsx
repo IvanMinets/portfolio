157668d2932f6481e962934647347ea5180198cb
Ivan Minets2 <minetssmash@gmail.com>
2023-06-19 13:33:31 +0300

    css updates

--- src/footer/Footer.tsx
+++ src/footer/Footer.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import style from './Footer.module.css'
+import styleContainer from "../common/styles/Container.module.css";
 
 const Footer = () => {
     return (
-        <div>
-            
+        <div className={style.footerBlock}>
+            <div className={`${styleContainer.container} ${style.footerContainer}`}>
+                <h2 className={style.footerTitle}>Ivan Minets</h2>
+                <div className={style.socialContainer}>
+                    <div className={style.social}></div>
+                    <div className={style.social}></div>
+                    <div className={style.social}></div>
+                    <div className={style.social}></div>
+                </div>
+                <span>Все права защищены</span>
+            </div>
         </div>
     );
 };
